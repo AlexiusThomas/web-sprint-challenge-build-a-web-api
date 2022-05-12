c1748c0fb60a18b3e347e77ad21cfc39d71aaf80
AlexiusVictoria <lexyvt@gmail.com>
2022-05-12 12:44:51 -0700

    testing123

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,57 @@
 // add middlewares here related to projects
+const Project = require('./projects-model')
+
+async function validateProjectId(req, res, next) {
+    try {
+        const id = await Project.get(req.params.id)
+        if (id) {
+            req.project = id
+            next()
+        } else {
+            next({ status: 404, message: "No project found" })
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+function validateProject(req, res, next) {
+    try {
+        const { name, description } = req.body
+        if (!name || !description) {
+            res.status(400).json({
+                message: "missing required info"
+            })
+            next()
+        } else {
+            next()
+        }
+    }
+    catch (err) {
+        next(err)
+    }
+}
+
+function validateProjectUpdate(req, res, next) {
+    try {
+        const { name, description, completed } = req.body
+        console.log(req.body)
+        if (!name || !description || completed === undefined) {
+            res.status(400).json({
+                message: "missing required info"
+            })
+            next()
+        } else {
+            next()
+        }
+    }
+    catch (err) {
+        next(err)
+    }
+}
+
+module.exports = {
+    validateProjectId,
+    validateProject,
+    validateProjectUpdate
+} 
--- api/server.js
+++ api/server.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const server = express();
+server.use(express.json())
 
+const projectsRouter = require('./projects/projects-router');
+const actionsRouter = require('./actions/actions-router');
+
+server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
+
+
+
+server.use('*', (req, res) => {
+    // catch all 404 errors middleware
+    res.json({ api: 'up' })
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status).json({
+      customMessage: 'something went wrong in router',
+      message: err.message,
+      stack: err.stack
+  });
+
+});
+  
 module.exports = server;
